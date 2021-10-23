726fded406fa189e74bbfa0d3a9ca9854e81d712
chvndler <70621588+chvndler@users.noreply.github.com>
2021-10-23 14:41:55 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,59 +4,68 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stage, Environment } from '@react-three/drei';
 import Model from '../components/model';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 
 export default function Index() {
-  const ref = useRef()
+    const ref = useRef()
 
-  return (
+    return (
 
-  <div className="canvas">
-    <Head>
-      <title>Atelier®</title>
-    </Head>
+<div className="canvas">
+  <Head>
+    <title>Atelier®</title>
+  </Head>
 
-    <div className="overlay">
-      (a®)
-      <a className="overlay__link" href="https://threejs.org"> three.js
-      </a>
-      <br></br>
-      view source on _
-      <a className="overlay__link" href="https://github.com/chvndler"> github
-      </a>
-      <br></br>
-      <br></br>
+  <div className="overlay">
+    (a®)
+    <a className="overlay__link" href="https://threejs.org">
+      {" "}
+      three.js
+    </a>
+    <br></br>
+    view source on _
+    <a className="overlay__link" href="https://github.com/chvndler">
+      {" "}
+      github
+    </a>
+    <br></br>
+    <br></br>
+    <Link href="/atelier">
       <a className="overlay__big" href="https://threejs.org">
         {" "}
         Atelier Design Yield®
       </a>
-
-    </div>
-    <div className="invisible lg:visible">
-      <Canvas shadows dpr={[1, 2]} camera={{ fov: 120 }}>
-        <Suspense fallback={null}>
-          <Environment background={true} files={'/hdr/park.hdr'} />
-          <Stage controls={ref} preset="soft" intensity={2} contactShadow={false} environment="park">
-            <Model />
-          </Stage>
-        </Suspense>
-        <OrbitControls ref={ref} autoRotate />
-      </Canvas>
+    </Link>
   </div>
 
+  {/* three.js Canvas (Desktop Only)*/}
+  <div className="invisible lg:visible">
+    <Canvas shadows dpr={[1, 2]} camera={{ fov: 120 }}>
+      <Suspense fallback={null}>
+        <Environment background={true} files={"/hdr/park.hdr"} />
+        <Stage
+          controls={ref}
+          preset="soft"
+          intensity={2}
+          contactShadow={false}
+          environment="park"
+        >
+          <Model />
+        </Stage>
+      </Suspense>
+      <OrbitControls ref={ref} autoRotate />
+    </Canvas>
+  </div>
+  {/* three.js canvas End */}
+
+  {/* Mobile Display */}
   <div className="visible lg:invisible">
     <div className="center__div">
-<Image
-  src="/svg/adyady.svg"
-  width={90}
-  height={120}
-  alt="back"
-/>
-</div>
+      <Image src="/svg/adyady.svg" width={90} height={120} alt="back" />
+    </div>
   </div>
 </div>
-
-
   );
 }
