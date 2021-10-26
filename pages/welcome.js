10227e9ebe69d108632a8d8149d7108d1928d92d
chvndler <70621588+chvndler@users.noreply.github.com>
2021-10-25 22:25:06 -0400

    Update welcome.js

--- pages/welcome.js
+++ pages/welcome.js
@@ -4,8 +4,21 @@ import Image from 'next/image';
 export default function Welcome() {
 
   return (
+<<<<<<< Updated upstream
   <>
 
+=======
+<>
+  <Head>
+    <title>Welcome</title>
+  </Head>
+  <Cover>
+    <Time />
+    <div className="center__div">
+      <Image src="/svg/brand.svg" alt="menu" width={100} height={50} />
+    </div>
+  </Cover>
+>>>>>>> Stashed changes
 </>
 
  );
