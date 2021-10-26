588e275c037e62660e9fbee2d6d5427e2bdd4843
chvndler <70621588+chvndler@users.noreply.github.com>
2021-10-25 22:23:55 -0400

    Update Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,26 @@
-import Head from 'next/head';
-import React, { Suspense, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage, Environment } from '@react-three/drei';
-import Material from '../components/material';
-import Time from '../components/time';
 import Image from 'next/image';
-import Link from 'next/link';
+import Cover from '../components/cover';
+import Time from '../components/time';
+import Head from 'next/head';
+
+
 
 
 
 export default function Index() {
-    const ref = useRef()
 
-    return (
-
-<div className="canvas">
+  return (
+<>
   <Head>
     <title>Atelier®</title>
   </Head>
-
-  <div className="overlay">
-    (ady®)
-    <br></br>
-    view source _
-    <a
-      className="overlay__link"
-      href="https://github.com/AtelierDesign/atelier"
-    >
-      {" "}
-      github
-    </a>
-
-
-      <div className="overlay__big">
-        <Time />
-        <Link
-          href="https://github.com/AtelierDesign/atelier"
-          target="_blank"
-          rel="noreferrer"
-          >
-          <a>Website Opening Soon.</a>
-        </Link>
-      </div>
-  </div>
-
-
-
-  {/*three.js Canvas (Desktop Only)*/}
-  <div className="invisible lg:visible">
-    <Canvas shadows dpr={[1, 2]} camera={{ fov: 120 }}>
-      <Suspense fallback={null}>
-        <Stage
-          controls={ref}
-          preset="soft"
-          intensity={3}
-          contactShadow={false}
-          environment="park"
-        >
-          <Material />
-        </Stage>
-      </Suspense>
-      <OrbitControls ref={ref} autoRotate />
-    </Canvas>
-  </div>
-  {/*three.js Canvas End*/}
-
-  {/*Mobile Only*/}
-  <div className="visible lg:invisible">
-    <Image
-      src="https://cdn.ady.systems/assets/ady_02.gif"
-      alt="cover"
-      layout="fill"
-      objectFit="cover"
-    />
-  </div>
-</div>
+  <Cover>
+    <Time />
+    <div className="center__div">
+      <Image src="/svg/brand.svg" alt="menu" width={100} height={50} />
+    </div>
+  </Cover>
+</>
 
   );
 }
--- pages/three.js
+++ pages/three.js
@@ -3,6 +3,7 @@ import React, { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stage, Environment } from '@react-three/drei';
 import Material from '../components/material';
+import Time from '../components/time';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -15,48 +16,64 @@ export default function Three() {
 
 <div className="canvas">
   <Head>
-    <title>Ady® Project.</title>
+    <title>three®</title>
   </Head>
 
   <div className="overlay">
-    (a®)
-    <a className="overlay__link" href="https://threejs.org">
-      {" "}
-      three.js
-    </a>
+    (ady®)
     <br></br>
-    view source on _
-    <a className="overlay__link" href="https://github.com/chvndler">
+    view source _
+    <a
+      className="overlay__link"
+      href="https://github.com/AtelierDesign/atelier"
+    >
       {" "}
       github
     </a>
-    <br></br>
-    <br></br>
-    <Link href="/atelier">
-      <a className="overlay__big" href="https://threejs.org">
-        {" "}
-        Atelier Design Yield®
-      </a>
-    </Link>
+
+
+      <div className="overlay__big">
+        <Time />
+        <Link
+          href="https://github.com/AtelierDesign/atelier"
+          target="_blank"
+          rel="noreferrer"
+          >
+          <a>Website Opening Soon.</a>
+        </Link>
+      </div>
   </div>
 
-  {/* three.js Canvas (Desktop Only)*/}
-  <Canvas shadows dpr={[1, 2]} camera={{ fov: 120 }}>
-    <Suspense fallback={null}>
-      <Environment background={false} files={"/hdr/park.hdr"} />
-      <Stage
-        controls={ref}
-        preset="soft"
-        intensity={3}
-        contactShadow={false}
-        environment="park"
-      >
-        <Material />
-      </Stage>
-    </Suspense>
-    <OrbitControls ref={ref} autoRotate />
-  </Canvas>
-  {/* three.js canvas End */}
+
+
+  {/*three.js Canvas (Desktop Only)*/}
+  <div className="invisible lg:visible">
+    <Canvas shadows dpr={[1, 2]} camera={{ fov: 120 }}>
+      <Suspense fallback={null}>
+        <Stage
+          controls={ref}
+          preset="soft"
+          intensity={3}
+          contactShadow={false}
+          environment="park"
+        >
+          <Material />
+        </Stage>
+      </Suspense>
+      <OrbitControls ref={ref} autoRotate />
+    </Canvas>
+  </div>
+  {/*three.js Canvas End*/}
+
+  {/*Mobile Only*/}
+  <div className="visible lg:invisible">
+    <Image
+      src="https://cdn.ady.systems/assets/ady_02.gif"
+      alt="cover"
+      layout="fill"
+      objectFit="cover"
+    />
+  </div>
 </div>
 
   );
