88068bed045f7d764d486e65937c6a8ed3cb4727
chvndler <70621588+chvndler@users.noreply.github.com>
2021-11-05 20:21:41 -0400

    yer

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  pringWidth: 120,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  parser: "babel",
+};
