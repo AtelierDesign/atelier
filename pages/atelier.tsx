31d865f24ed8ea991a95992b0d97cff9d5cd23de
chvndler <70621588+chvndler@users.noreply.github.com>
2021-11-12 15:53:54 -0500

    Update atelier.tsx

--- pages/atelier.tsx
+++ pages/atelier.tsx
@@ -54,7 +54,7 @@ const Card = styled('div', {
 
 const Atelier = () => {
   return (
-    <div css={{ bc: '$slate1' }}>
+    <div>
       <Box css={{ bc: '$slate1', marginTop: '0', marginLeft: '0', marginRight: '0', px: '0', paddingBottom: '30px' }}>
         <NavbarSticky />
         <Hero />
