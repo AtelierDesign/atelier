54f0f2a0d5f69e4dd68525f2b35b95f692a6303e
chvndler <70621588+chvndler@users.noreply.github.com>
2021-11-13 14:26:54 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ module.exports = withTM();
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.ady.systems', 'ady.systems', 'images.unsplash.com']
+    domains: ['cdn.ady.systems', 'ady.systems', 'images.unsplash.com'],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp', 'image/png', 'image/jpg']
   },
   experimental: {
     urlImports: ['https://cdn.ady.systems/', 'https://cdn.skypack.dev/']
