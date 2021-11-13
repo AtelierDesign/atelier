cb54ac87907dc3fd0d039b6fb865c9b28ba8669b
chvndler <70621588+chvndler@users.noreply.github.com>
2021-11-13 14:29:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.ady.systems', 'ady.systems', 'images.unsplash.com'],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp', 'image/png', 'image/jpg']
   },
   experimental: {
