111fc97f90311c83201a39479b8ff9011b0729d6
chvndler <chan@ady.world>
2021-12-27 11:03:17 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ module.exports = withTM();
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.ady.systems', 'ady.systems', 'images.unsplash.com']
+    domains: ['cdn.ady.systems', 'ady.systems', 'images.unsplash.com'],
   },
   experimental: {
-    urlImports: ['https://cdn.ady.systems/', 'https://cdn.skypack.dev/']
-  }
+    urlImports: ['https://cdn.ady.systems/', 'https://cdn.skypack.dev/'],
+  },
 };
