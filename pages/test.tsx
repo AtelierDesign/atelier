a568d99b9b9ced125236c2c3d581743315ee2e57
chvndler <chan@ady.world>
2022-01-12 16:31:25 -0500

    BACK

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+import { GradientBackground } from '@components/Background/GradientBackground';
+import { Text } from '@atelier/text';
+import React from 'react';
+
+export default function Test() {
+  return (
+    <>
+      <GradientBackground>
+        <Text> HEY </Text>
+      </GradientBackground>
+    </>
+  );
+}
+
+// export default Test;
