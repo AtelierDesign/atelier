223bbbd7c15733ec7dc8eb8cb3d5111c27caea23
chvndler <chan@ady.world>
2022-01-13 10:01:07 -0500

    run theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { Box, box } from '@atelier/box';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
 import { ThemeProvider } from 'next-themes';
 
 // STITCHES.DEV
-import { css, globalCss } from 'stitches.config';
+import { css, globalCss, darkTheme } from 'stitches.config';
 import { reset } from '@styles/reset';
 
 // MAIN STYLE IMPORTS
@@ -19,22 +20,49 @@ import '../styles.css';
 // PROGRESS LOADER
 import NextNprogress from 'nextjs-progressbar';
 
+const globalStyles = globalCss({
+  html: {
+    overflowX: 'hidden',
+  },
+  body: {
+    margin: 0,
+    backgroundColor: '$slate1',
+  },
+});
+
 const appWrapper = css({
   include: ['box', 'minHeightScreen'],
 });
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  globalStyles();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
 
   globalCss(reset, {
     body: {
       backgroundColor: '$slate1',
       fontFamily: '$inter',
     },
+    '[data-theme="dark"]': {
+      fg: '#fff',
+      bg: '#000',
+    },
   });
 
   return (
-    <ThemeProvider defaultTheme="system" enableSystem={true} attribute="class">
+    <ThemeProvider
+      disableTransitionOnChange
+      attribute="class"
+      value={{ light: 'light-theme', dark: darkTheme.className }}
+      defaultTheme="system">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
       </Head>
