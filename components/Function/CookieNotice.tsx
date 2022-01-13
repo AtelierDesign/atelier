95bb6202a0906fd7d565b19478ecbd53cecb2b61
chvndler <chan@ady.world>
2022-01-12 21:57:34 -0500

    Update CookieNotice.tsx

--- components/Function/CookieNotice.tsx
+++ components/Function/CookieNotice.tsx
@@ -9,17 +9,19 @@ export const CookieNotice = () => {
 
   return (
     <CookieConsent
-      debug={true}
+      debug={false}
       expires={150}
       location="bottom"
       buttonText="Accept"
       cookieName="AtelierCookie001"
       style={{
+        display: 'flex',
+        flexDirection: 'row',
         border: '1px solid #231F20',
         borderTopWidth: '1px',
-        borderBottomWidth: 'none',
-        borderLeftWidth: 'none',
-        borderRightWidth: 'none',
+        borderBottomWidth: '0',
+        borderLeftWidth: '0',
+        borderRightWidth: '0',
         background: 'rgba(240, 240, 240, 0.5)',
         backdropFilter: 'blur(10px)',
         WebkitBackdropFilter: 'blur(10px)',
@@ -29,22 +31,31 @@ export const CookieNotice = () => {
         paddingBottom: '10px',
         paddingLeft: '6px',
         paddingRight: '6px',
+
+        '@500px': {
+          flexDirection: 'column',
+        },
       }}
       buttonStyle={{
         backgroundColor: 'hsl(81, 67%, 50%)',
         color: '#FFFFFF',
         fontSize: '13px',
         paddingTop: '6px',
-        paddingBottom: '6px',
+        paddingBottom: '8px',
         paddingLeft: '10px',
         paddingRight: '10px',
         borderRadius: '6px',
         fontFamily: "'Inter', sans-serif'",
         fontWeight: '600',
       }}>
-      <Text size="2" css={{ fontWeight: '500' }}>
-        A Cookies Policy is used to inform your site's visitors that you're using cookies on your website, web app, or
-        mobile app.
+      <Text css={{ fontFamily: '$neueplak', fontSize: '12px', fontWeight: 'bold', lineHeight: '1.3' }}>
+        We use cookies.
+      </Text>
+      <Text size="2" css={{ fontWeight: '500', width: '70vw', fontSize: '11px', lineHeight: '1.3' }}>
+        We use cookies to personalise content and ads, to provide social media features and to analyse our traffic. We
+        also share information about your use of our site with our social media, advertising and analytics partners who
+        may combine it with other information that you’ve provided to them or that they’ve collected from your use of
+        their services.
       </Text>
     </CookieConsent>
   );
