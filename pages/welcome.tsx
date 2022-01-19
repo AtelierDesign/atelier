a5272ddc6de9a5012581f16ee4cab637ec631c2f
chvndler <chan@ady.world>
2022-01-19 18:21:24 -0500

    Create welcome.tsx

---
+++ pages/welcome.tsx
@@ -0,0 +1,108 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import NextLink from 'next/link';
+import fourImage from '../public/img/four.png';
+import { NormalLink } from '@atelier/normal-link';
+import { Text } from '@atelier/text';
+import { Box } from '@atelier/box';
+import { SmallSpacer } from '@components/SmallSpacer';
+import { CoverFooter } from '@components/CoverFooter';
+
+import { styled } from '@stitches/react';
+
+const CoverBox = styled('div', {
+  zIndex: '10',
+  width: '100vw',
+  minWidth: '100vw',
+  height: '100%',
+  backgroundColor: 'rgba(240, 240, 240, 0.3)',
+
+  // RADIAL GRADIENT
+  /*
+  backgroundImage: `
+  radial-gradient(circle 300px at 500px 200px, $blue8, $lime9Alpha),
+  radial-gradient(circle 500px at calc(100% - 900px) 300px, $lime6, $lime9Alpha),
+  radial-gradient(circle 800px at left center, $blue6, $sky9Alpha)
+  `,
+*/
+  display: 'block',
+  position: 'relative',
+  margin: 'auto',
+  opacity: '0.9',
+  backdropFilter: 'invert(10%) blur(20px)',
+  saturate: '300%',
+
+  overflowY: 'hidden',
+  overflowX: 'hidden',
+});
+
+const TextBox = styled('div', {
+  zIndex: '200',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '100%',
+  textAlign: 'center',
+  margin: 'auto',
+  padding: '1rem',
+});
+
+// APP BEGIN / WELCOME (Newsletter Success Redirect)
+const Welcome = () => {
+  return (
+    <>
+      <Box
+        css={{
+          padding: '0px',
+          height: '100vh',
+          zIndex: '0',
+          overflowY: 'hidden',
+          overflowX: 'hidden',
+          backgroundColor: '$slate1',
+        }}>
+        <Head>
+          <title>Atelier®.</title>
+        </Head>
+
+        <CoverBox>
+          <CoverFooter />
+          <TextBox>
+            <NextLink href="/ady-index" passHref>
+              <NormalLink variant="normal">
+                <Text
+                  css={{
+                    fontFamily: '$neuewide',
+                    fontSize: '20px',
+                    fontWeight: '900',
+                    letterSpacing: '-0.05rem',
+                    color: '$gray12',
+
+                    '&:hover': {
+                      color: '$gray10',
+                    },
+                  }}>
+                  ATELIER DESIGN YIELD®
+                </Text>
+                <Text css={{ fontWeight: '600', fontSize: '12px', lineHeight: '1.3' }}>
+                  An ongoing [ <strong>Web3</strong> ] project.
+                </Text>
+                {/*
+              <Text css={{ fontWeight: '500', fontSize: '12px', lineHeight: '1.2' }}>
+                <strong>ADY</strong>®Store Opening Soon. An ongoing [Web3] prjct. © 2022
+              </Text>
+              */}
+                {/* <!-- HOLD LOGO <Image src="/logo.svg" alt="One" width={120} height={60} /> --> */}
+              </NormalLink>
+            </NextLink>
+          </TextBox>
+        </CoverBox>
+        <Image src={fourImage} alt="One" layout="fill" objectFit="cover" />
+        {/* <!--   <Image src="https://cdn.ady.systems/ady.images/btrfly.png" alt="One" layout="fill" objectFit="cover" /> --> */}
+      </Box>
+    </>
+  );
+};
+
+export default Welcome;
