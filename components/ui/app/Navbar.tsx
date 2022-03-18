a6098b2600aa7a54a255210bc9c02499bd954985
chvndler <chan@ady.world>
2022-03-18 17:15:55 -0400

    ghgh

--- components/ui/app/Navbar.tsx
+++ components/ui/app/Navbar.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Router from 'next/router';
 import NextLink from 'next/link';
 
+import { supabase } from '@lib/client';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 // ATELIER® DESIGN SYSTEM
 import { Box } from '@atelier/box';
 import { Flex } from '@atelier/flex';
@@ -11,7 +15,75 @@ import { ThemeSwitch } from '@ui/app/ThemeSwitch';
 import { PopoverPanel } from '@fnctns/PopoverPanel';
 import { CaretRightIcon } from '@radix-ui/react-icons';
 
+// Styled..
+import { styled } from 'stitches.config';
+
+const Nav = styled('div', {
+  position: 'fixed',
+  top: '0',
+  minWidth: '100vw',
+  margin: 'auto',
+  alignItems: 'left',
+  padding: '10px',
+  paddingTop: '20px',
+  zIndex: '9999',
+
+  height: '160px',
+});
+
+const LinkStyle = styled('div', {
+  display: 'inline',
+  margin: '12px',
+  lineHeight: '1',
+  paddingTop: '6px',
+  paddingBottom: '6px',
+  paddingLeft: '0px',
+  paddingRight: '0px',
+
+  '&:hover': {
+    cursor: 'pointer',
+  },
+});
+
 export const Navbar = () => {
+  // Set Authenticated
+  const router = useRouter();
+  const [authenticatedState, setAuthenticatedState] = useState('not-authenticated');
+
+  // Supa
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      handleAuthChange(event, session);
+      if (event === 'SIGNED_IN') {
+        setAuthenticatedState('authenticated');
+        router.push('/profile');
+      }
+      if (event === 'SIGNED_OUT') {
+        setAuthenticatedState('not-authenticated');
+      }
+    });
+    checkUser();
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, []);
+
+  async function checkUser() {
+    const user = await supabase.auth.user();
+    if (user) {
+      setAuthenticatedState('authenticated');
+    }
+  }
+
+  async function handleAuthChange(event, session) {
+    await fetch('/api/auth', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      credentials: 'same-origin',
+      body: JSON.stringify({ event, session }),
+    });
+  }
+
   return (
     <>
       <Flex
@@ -116,7 +188,7 @@ export const Navbar = () => {
 
           {/* <!-- JSX ELEMENT --> */}
           <Link
-            href="/login"
+            href="/sign-in"
             // target="_blank"
             // rel="norefferer"
             css={{
---
+++ pages/sign-in.tsx
@@ -0,0 +1,121 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { TextField } from '@atelier/text-field';
+import { Heading } from '@atelier/heading';
+import { Container } from '@atelier/container';
+import { Box } from '@atelier/box';
+import { Button } from '@atelier/Button';
+import { GradientButton, AtelierButton } from '@ui/buttons/AtelierButton';
+import { SmallSpacer } from '@components/SmallSpacer';
+import { Card } from '@components/Card';
+import { ChevronRightIcon } from '@radix-ui/react-icons';
+import { TwitterLogoIcon } from '@radix-ui/react-icons';
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+// import { BsApple } from 'react-icons/bs';
+
+import { supabase } from '@lib/client';
+
+import { styled } from 'stitches.config';
+
+const CenterBox = styled('div', {
+  minHeight: '100vh',
+  padding: '0 0.5rem',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+});
+
+export default function SignIn() {
+  // EMAIL
+  const [email, setEmail] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+  const [session, setSession] = useState(null);
+
+  // AUTH HANDLERS
+  // Magic Link
+  async function signIn() {
+    const { user, session, error } = await supabase.auth.signIn({
+      email,
+    });
+
+    if (error) {
+      console.log({ error });
+    } else {
+      setSubmitted(true);
+    }
+  }
+
+  // Continue with Twitter
+  async function signInWithTwitter() {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'twitter',
+    });
+  }
+
+  // Continue with GitHub
+  async function signInWithGithub() {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'github',
+    });
+  }
+
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+  }
+
+  if (submitted) {
+    return (
+      <Box css={{ height: '100vh' }}>
+        <CenterBox>
+          <Container size="3">
+            <Heading
+              size="1"
+              css={{
+                fontFamily: 'Helvetica',
+                fontSize: '13px',
+                fontWeight: '700',
+                paddingBottom: '20px',
+                color: '$gray12',
+              }}>
+              HEAD TO YOUR INBOX TO SECURELY SIGN IN.
+            </Heading>
+          </Container>
+        </CenterBox>
+      </Box>
+    );
+  }
+
+  return (
+    <Box css={{ height: '100vh' }}>
+      <CenterBox>
+        <Heading size="3" css={{ fontFamily: '$inter', paddingBottom: '20px' }}>
+          Sign in
+        </Heading>
+
+        <Container size="3" css={{ color: '$gray12' }}>
+          <TextField size="2" onChange={e => setEmail(e.target.value)} type="email" placeholder="Enter email address" />
+          <AtelierButton color="atelier" onClick={() => signIn()}>
+            Submit <ChevronRightIcon />
+          </AtelierButton>
+        </Container>
+
+        <SmallSpacer />
+
+        <Container size="3" css={{ alignItems: 'center', textAlign: 'center' }}>
+          <AtelierButton color="twitter" size="3" onClick={signInWithTwitter}>
+            <TwitterLogoIcon /> Continue with Twitter
+          </AtelierButton>
+        </Container>
+
+        <Container size="3" css={{ alignItems: 'center', textAlign: 'center', paddingTop: '6px' }}>
+          <AtelierButton color="github" size="3" onClick={signInWithGithub}>
+            <GitHubLogoIcon /> Continue with GitHub
+          </AtelierButton>
+        </Container>
+      </CenterBox>
+    </Box>
+  );
+}
